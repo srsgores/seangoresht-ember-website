22bc838e22234b4e7795ac582fd4750365709af3
Sean Goresht <seangores@gmail.com>
2021-12-09 14:46:51 -0700

    chore: reformat code

--- app/router.js
+++ app/router.js
@@ -1,9 +1,10 @@
-import EmberRouter from '@ember/routing/router';
-import config from 'seangoresht-blog/config/environment';
+import EmberRouter from "@ember/routing/router";
+import config from "seangoresht-blog/config/environment";
 
 export default class Router extends EmberRouter {
-  location = config.locationType;
-  rootURL = config.rootURL;
+	location = config.locationType;
+	rootURL = config.rootURL;
 }
 
-Router.map(function () {});
+Router.map(function() {
+});
--- tests/test-helper.js
+++ tests/test-helper.js
@@ -1,9 +1,9 @@
-import Application from 'seangoresht-blog/app';
-import config from 'seangoresht-blog/config/environment';
-import * as QUnit from 'qunit';
-import { setApplication } from '@ember/test-helpers';
-import { setup } from 'qunit-dom';
-import { start } from 'ember-qunit';
+import Application from "seangoresht-blog/app";
+import config from "seangoresht-blog/config/environment";
+import * as QUnit from "qunit";
+import {setApplication} from "@ember/test-helpers";
+import {setup} from "qunit-dom";
+import {start} from "ember-qunit";
 
 setApplication(Application.create(config.APP));
 
