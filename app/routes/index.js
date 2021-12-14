19024102828fbabd54f9e269a4be6046cb9cbadd
Sean Goresht <seangores@gmail.com>
2021-12-14 03:05:31 -0700

    chore: remove supabase

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,10 +1,3 @@
 import Route from "@ember/routing/route";
-import {inject as service} from "@ember/service";
 
-export default class IndexRoute extends Route {
-	@service supabase;
-	async model() {
-		let {data} = await this.supabase.db.from("posts").select();
-		return data;
-	}
-}
+export default class IndexRoute extends Route {}
