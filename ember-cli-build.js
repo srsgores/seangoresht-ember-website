9ea154bec77212cf2d48ed2773acefe9305a3a4a
Sean Goresht <seangores@gmail.com>
2021-12-14 03:48:54 -0700

    fix: wrong image path

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,11 +1,18 @@
-'use strict';
+"use strict";
 
-const EmberApp = require('ember-cli/lib/broccoli/ember-app');
+const EmberApp = require("ember-cli/lib/broccoli/ember-app");
 
-module.exports = function (defaults) {
-  let app = new EmberApp(defaults, {
-    // Add options here
-  });
+module.exports = function(defaults) {
+	let app = new EmberApp(defaults, {
+		"responsive-image": {
+			images: [
+				{
+					include: "assets/images/**/*",
+					widths: [2048, 1536, 1080, 750, 640]
+				}
+			]
+		}
+	});
 
-  return app.toTree();
+	return app.toTree();
 };
