95d9d03c399bff81df04dff5498564f3d622c5df
Sean Goresht <seangores@gmail.com>
2021-12-15 14:21:01 -0700

    feat: overlayed image

--- app/components/overlayed-image.js
+++ app/components/overlayed-image.js
@@ -1,7 +1,11 @@
 import Component from "@glimmer/component";
+import {htmlSafe} from "@ember/template";
 
 export default class OverlayedImageComponent extends Component {
 	get alt() {
 		return this.args.alt || "";
+	}
+	get offsetStyle() {
+		return this.args.offset ? htmlSafe(`--top-offset: ${this.args.offset};`) : null;
 	}
 }
