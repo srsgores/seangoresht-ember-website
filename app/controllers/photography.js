6d13119a227332ffcc21be5dc2b05a04b064c0e6
Sean Goresht <seangores@gmail.com>
2021-12-14 19:41:59 -0700

    feat: finish up photography nature section

--- app/controllers/photography.js
+++ app/controllers/photography.js
@@ -17,6 +17,53 @@ export default class PhotographyController extends Controller {
 		{
 			path: "nature/budding-lilac.jpg",
 			name: "Budding Lilac"
+		},
+		{
+			path: "nature/dandruff-weed.jpg",
+			name: "Dandruff Weed"
+		},
+		{
+			path: "nature/deer-grazing.jpg",
+			name: "Deer Grazing"
+		},
+		{
+			path: "nature/finch-river.jpg",
+			name: "House Finch Perched along Elbow River"
+		},
+		{
+			path: "nature/finch2.jpg"
+		},
+		{
+			path: "nature/flower-uncropped.jpg",
+			name: "Flower"
+		},
+		{
+			path: "nature/fly-on-leaf.jpg",
+			name: "Fly perched on top of leaf"
+		},
+		{
+			path: "nature/geese.jpg",
+			name: "Canadian Geese"
+		},
+		{
+			path: "nature/leaves.jpg",
+			name: "Leaves"
+		},
+		{
+			path: "nature/raspberry.jpg",
+			name: "Canadian Raspberry"
+		},
+		{
+			path: "nature/red-flowers.jpg",
+			name: "Red Flowers"
+		},
+		{
+			path: "nature/robin-chirping.jpg",
+			name: "Robin Chirping"
+		},
+		{
+			path: "nature/yellow-flowers.jpg",
+			name: "Yellow Flowers"
 		}
 	]
 }
