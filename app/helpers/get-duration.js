ae65827b9522a65f3b61346cc3836d8d65bda4d2
Sean Goresht <seangores@gmail.com>
2023-10-24 18:05:43 -0600

    feat: add get-duration helper

---
+++ app/helpers/get-duration.js
@@ -0,0 +1,33 @@
+import {helper} from "@ember/component/helper";
+import {assert, warn} from "@ember/debug";
+
+export const MISSING_POSITIONAL_PARAM_ERROR_MESSAGE = "get-duration called with no positional params!";
+export const MISSING_DATE_ERROR_MESSAGE = "get-duration helper requires a date";
+
+export const DATE_AFTER_TODAY_WARNING_MESSAGE = "get-duration helper called with date after today.  This will result in a negative duration.  Did you mean this?";
+export const TRANSLATIONS = {
+	year:  "year",
+	month: "month"
+};
+
+export default helper(function getDuration(positional/*, named*/) {
+	const [date] = positional;
+	const now = new Date();
+	assert(MISSING_POSITIONAL_PARAM_ERROR_MESSAGE, date);
+	assert(`${MISSING_DATE_ERROR_MESSAGE}; got ${typeof date}`, date instanceof Date);
+	warn(DATE_AFTER_TODAY_WARNING_MESSAGE, date < now, {id: "get-duration.invalid-date"});
+	const years = now.getFullYear() - date.getFullYear();
+	const months = now.getMonth() - date.getMonth();
+
+	let duration = "";
+
+	if (years > 0) {
+		duration = `${years} ${TRANSLATIONS.year}${years > 1 ? 's' : ''}`;
+	}
+
+	if (months > 0) {
+		duration += `${years > 0 ? ', ' : ''}${months} ${TRANSLATIONS.month}${months > 1 ? 's' : ''}`;
+	}
+
+	return duration;
+});
---
+++ tests/integration/helpers/get-duration-test.js
@@ -0,0 +1,58 @@
+import {module, test} from "qunit";
+import {setupRenderingTest} from "ember-qunit";
+import {render, setupOnerror} from "@ember/test-helpers";
+import {MISSING_DATE_ERROR_MESSAGE, MISSING_POSITIONAL_PARAM_ERROR_MESSAGE} from "seangoresht-blog/helpers/get-duration";
+import {hbs} from "ember-cli-htmlbars";
+
+const ERROR_PREFIX = "Assertion Failed: ";
+
+module("Integration | Helper | get-duration", function (hooks) {
+	setupRenderingTest(hooks);
+
+	test("it throws an error if no positional arguments provided", async function (assert) {
+		setupOnerror(function (error) {
+			assert.equal(error.message, `${ERROR_PREFIX}${MISSING_POSITIONAL_PARAM_ERROR_MESSAGE}`);
+		});
+		await render(hbs`{{get-duration}}`);
+	});
+
+	test("it throws an error if positional argument not a date", async function (assert) {
+		this.startDate = new Date().getFullYear();
+		setupOnerror(function (error) {
+			assert.ok(error.message.includes(`${ERROR_PREFIX}${MISSING_DATE_ERROR_MESSAGE}`));
+		});
+		await render(hbs`{{get-duration this.startDate}}`);
+	});
+
+	test("it subtracts the date start year from today's year", async function (assert) {
+		const today = new Date();
+		const thisYear = new Date().getFullYear();
+		const lastYearDate = new Date(thisYear - 1, today.getMonth(), today.getDate());
+		const lastYear = thisYear - 1;
+		this.startDate = lastYearDate;
+		const expectedDurationInYears = `${thisYear - lastYear} year`;
+
+		await render(hbs`{{get-duration this.startDate}}`);
+		assert.dom(this.element).hasText(expectedDurationInYears);
+	});
+
+	test("it pluralizes years if duration is greater than 1", async function (assert) {
+		const today = new Date();
+		const thisYear = today.getFullYear();
+		const yearDifference = 2;
+		this.startDate = new Date(thisYear - yearDifference, today.getMonth(), today.getDate());
+
+		await render(hbs`{{get-duration this.startDate}}`);
+		assert.dom(this.element).hasText(`${yearDifference} years`);
+	});
+
+	test("it pluralizes months if duration is greater than 1", async function (assert) {
+		const today = new Date();
+		const thisYear = today.getFullYear();
+		const monthDifference = 2;
+		this.startDate = new Date(thisYear, today.getMonth() - monthDifference, today.getDate());
+
+		await render(hbs`{{get-duration this.startDate}}`);
+		assert.dom(this.element).hasText(`${monthDifference} months`);
+	});
+});
