dfd1692dd0727c2ac8a15610cb105b6bb79e8bc1
Sean Goresht <seangores@gmail.com>
2023-10-26 23:17:18 -0600

    test(helpers): add test for supporting end-date on get-duration

--- tests/integration/helpers/get-duration-test.js
+++ tests/integration/helpers/get-duration-test.js
@@ -55,4 +55,16 @@ module("Integration | Helper | get-duration", function (hooks) {
 		await render(hbs`{{get-duration this.startDate}}`);
 		assert.dom(this.element).hasText(`${monthDifference} months`);
 	});
+
+	test("It calculates the duration from the supplied end date", async function (assert) {
+		const today = new Date();
+		const thisYear = today.getFullYear();
+		const monthDifference = 2;
+		this.endDate = new Date(thisYear, today.getMonth() - monthDifference, today.getDate());
+		this.startDate = new Date(thisYear - 1, today.getMonth(), today.getDate());
+		const expectedDurationInMonths = `${monthDifference} months`;
+
+		await render(hbs`{{get-duration this.startDate this.endDate}}`);
+		assert.dom(this.element).hasText(expectedDurationInMonths);
+	});
 });
